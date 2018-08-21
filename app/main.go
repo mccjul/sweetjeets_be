d6f9aeb8ee5eb3fbdf1080d419c7e4c1640c2150
Dethbug <mccjul@gmail.com>
2018-08-21 11:49:31 -0400

    Non relay based serve

--- app/main.go
+++ app/main.go
@@ -2,16 +2,43 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	graphql "github.com/graph-gophers/graphql-go"
-	"github.com/graph-gophers/graphql-go/relay"
 )
 
 type query struct{}
+
+// GraphQL struct for apollo
+type GraphQL struct {
+	Schema *graphql.Schema
+}
+
+func (h *GraphQL) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var params struct {
+		Query         string                 `json:"query"`
+		OperationName string                 `json:"operationName"`
+		Variables     map[string]interface{} `json:"variables"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response := h.Schema.Exec(r.Context(), params.Query, params.OperationName, params.Variables)
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseJSON)
+}
 
 func (q *query) Hello() string { return "Hello, world!" }
 
@@ -29,6 +56,12 @@ func getFile(path string) (string, error) {
 	return string(b), nil
 }
 
+func serveGQL(ctx context.Context, h GraphQL) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
 func main() {
 	s, err := getFile("./schema.graphql")
 	page, err := getFile("./graphql.html")
@@ -36,8 +69,10 @@ func main() {
 		return
 	}
 
-	schema := graphql.MustParseSchema(s, &query{})
-	http.Handle("/query", &relay.Handler{Schema: schema})
+	gqlschema := graphql.MustParseSchema(s, &query{})
+	ex := GraphQL{Schema: gqlschema}
+	ctx := context.Background()
+	http.Handle("/query", serveGQL(ctx, ex))
 
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(page))
